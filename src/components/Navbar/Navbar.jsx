219583d6412982970d4ccc7d60c13ff07cad611d
lordkendri <76025241+lordkendri@users.noreply.github.com>
2022-04-04 23:24:58 -0400

    Modified_smallMenu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       <a href="/" className="p__opensans">Book Table</a>
     </div>
     <div className="app__navbar-smallscreen"> 
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={()=> setToggleMenu(true)} />  
+      <GiHamburgerMenu className="app__navbar-Gi"  fontSize={27} onClick={()=> setToggleMenu(true)} />  
       {toggleMenu &&(
         <div className="app__navbar-smallscreen_overlay flex__center slide-bottom"> 
           <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=> setToggleMenu(false)} />
